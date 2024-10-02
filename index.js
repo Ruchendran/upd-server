d7b9e2c3eaa623988102c051af5cc4838e60deed
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2024-10-02 19:23:00 +0530

    first setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const dotenv=require("dotenv");
+dotenv.config({path:'./config.env'});
+const cors=require("cors");
+const express=require("express");
+const path=require("path");
+const {open}=require("sqlite");
+const sqlite3=require("sqlite3");
+const index=express();
+index.use(cors());
+index.use(express.json());
+let db=null;
+
+const dbPath=path.join(__dirname,"users.db");
+
+const port=process.env.port || 300
+
+
+initiate=async()=>{
+    try{
+        db=await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        })
+        index.listen(port,()=>{
+            console.log(`server Runnning at  ${port}`)
+        })
+    }
+    catch(e){
+        console.log(`DB error:${e.message}`);
+        process.exit(1);
+    }
+}
+
+
+initiate();
+
+index.get("/get",async(request,response)=>{
+    const que=`
+    select * from lead
+    `;
+    const data=await db.all(que);
+    console.log(data);
+    response.send(data)
+})
